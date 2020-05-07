be6e5d8545788951e54dcbaaf4924c494a8af32d
fuad2101 <fuady.teknikinformatika@gmail.com>
2020-05-07 16:26:29 +0800

    Add file CC.php Footer.php format_sms.php and header.php

---
+++ cc.php
@@ -0,0 +1,51 @@
+<?php
+require ('header.php')
+?>
+
+<div class="container">
+    <h3>Call Center</h3>
+
+    <!-- Lion Air -->
+    <ol>
+        <h4>Lion Group : 0804.1.77.88.99</h4>
+        <p>Extention:</p>
+        <li>Tiketing</li>
+        <li>Business Class</li>
+        <li>Batik Frequent Flyer</li>
+        <li>Refund/Reschedule</li>
+        <li>Lion Parcel</li>
+        <li>Informasi & Keluhan Pelanggan</li>
+    </ol>
+
+
+    <!-- Sriwijaya -->
+    <ol>
+        <h4>Sriwijaya Group : 0804.1.77.77.77</h4>
+    </ol
+    >
+    <!-- Citilink -->
+    <ol>
+        <h4>Citilink Group : 0804.1.08.08.08</h4>
+    </ol>
+
+    <!-- Garuda -->
+    <ol>
+        <h4>Garuda Indonesia : 0804.180.78.07</h4>
+    </ol>
+
+    <!-- Pelni -->
+    <ol>
+        <h4>Pelni : (021) 162</h4>
+        <p>Extention:</p>
+        <li>PT.Pelni</li>
+        <li>Logistik</li>
+    </ol>
+
+    <p>Cabang Makassar</p>
+    <p>Lion: 0411-368777/888</p>
+    <p>Sriwijaya: 0411-424700</p>
+    <p>Citilink (Bandara Makassar): 0411-3656096</p>
+    <p>Garuda: 0411-3654747</p>
+
+
+</div>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="ism/css/bootstrap.css"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>BMW Tools</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">BMW Tools</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="tools.php">Refund/Reschedule <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="format_sms.php">Format SMS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cc.php">Call Center</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
