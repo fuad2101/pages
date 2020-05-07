c25172c95bde78e07803ee0d557fc4ba830d6d7b
fuad2101 <fuady.teknikinformatika@gmail.com>
2020-05-07 21:29:12 +0800

    Add Info

---
+++ hajiumrah.php
@@ -0,0 +1,12 @@
+<?php
+include 'header.php'
+?>
+
+<div class="container">
+    <h1>Pendaftaran umrah</h1>
+    <ol>
+        <li>F.C KTP</li>
+        <li>Passport asli yang masih berlaku minimal 5 bulan sebelum jadwal keberangkatan </li>
+        <li>Batas waktu pendaftaran minimal</li>
+    </ol>
+</div>
