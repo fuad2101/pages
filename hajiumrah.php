e66ea451094f72e191a11005b269e23d0ebc7b50
fuad2101 <fuady.teknikinformatika@gmail.com>
2020-06-25 15:14:46 +0800

    Update info

--- hajiumrah.php
+++ hajiumrah.php
@@ -7,6 +7,7 @@ include 'header.php'
     <ol>
         <li>F.C KTP</li>
         <li>Passport asli yang masih berlaku minimal 5 bulan sebelum jadwal keberangkatan </li>
-        <li>Batas waktu pendaftaran minimal</li>
+        <li>Batas waktu pendaftaran minimal 1 bulan sebelum jadwal keberangatan dan pembayaran harus lunas</li>
+        <li>Pembayaran dengan sistem cicil, pelunasan paling lambat 35 hari sebelum jadwal keberangkatan</li>
     </ol>
 </div>
